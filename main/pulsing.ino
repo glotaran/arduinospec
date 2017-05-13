3236ba0c549790231c924dfdd87b72230efbb130
Joris Snellenburg <j.snellenburg@vu.nl>
2017-05-13 23:49:57 +0200

    Adding Joern's code for working prototype
    This is code to control the timing of 2 or multiple LEDs as well as
    trigger spectral aqusition of a spectrometer.
    
    Some additional info links to improve the timings:
    https://forum.arduino.cc/index.php?topic=99336.15
    http://forum.arduino.cc/index.php?topic=288234.0
    http://forum.arduino.cc/index.php?topic=225329

--- main/pulsing.ino
+++ main/pulsing.ino
@@ -1,19 +1,31 @@
-int nrStates;
+unsigned int nrStates = 0;
 byte states [100];
 long delays [100];
+const byte triggerBit = 7;
 
 void do_state(int state) {
   set_pins(states[state]);
-  
-  long t = 0;
-  while (t < delays[state]) {
-    t += do_trigger();
-  }  
+
+  if (bitRead(states[state], triggerBit)) {
+    long t = 0;
+    while (t < delays[state]) {
+      t += do_trigger();
+    }
+  } else {
+    delay(delays[state]);
+  }
 }
 
-void fire(){
+void fire(byte first, byte last) {
   set_once();
-  for (int i = 0; i<nrStates; i++){
+  for (int i = first; i <= last; i++) {
     do_state(i);
+  }
+  set_pins(0);
+}
+
+void fire_n(int n, byte first, byte last) {
+  for (int i = 0; i < n; i++) {
+    fire(first, last);
   }
 }
--- main/serial_reactor.ino
+++ main/serial_reactor.ino
@@ -21,89 +21,113 @@ const byte GET_STATE = 0x0C;
 const byte RESET_ONCE = 0x0D;
 
 const byte FIRE = 0x0E;
+const byte FIRE_N = 0x0F;
 
 void setup_serial() {
-    Serial.begin(9600);
+  Serial.begin(9600);
 }
 
 void process_command(byte cmd) {
-  switch(cmd) {
-  case SET_TRIGGER_WIDTH:
-    Serial.println("OK");
-    trigWidth = Serial.parseInt();
-    Serial.println("OK");
-    break;
-  case GET_TRIGGER_WIDTH:
-    Serial.print("OK ");
-    Serial.println(trigWidth);
-    break;
-  case SET_TRIGGER_PERIOD:
-    Serial.println("OK");
-    trigPeriod = Serial.parseInt();
-    Serial.println("OK");
-    break;
-  case GET_TRIGGER_PERIOD:
-    Serial.print("OK ");
-    Serial.println(trigPeriod);
-    break;
-  case SET_TRIGGER_OUT_PINS:
-    Serial.println("OK");
-    triggers = Serial.parseInt();
-    Serial.println("OK");
-    break;
-  case GET_TRIGGER_OUT_PINS:
-    Serial.print("OK ");
-    Serial.println(triggers);
-    break;
-  case SET_ONCE_OUT_PINS:
-    Serial.println("OK");
-    setOnce = Serial.parseInt();
-    Serial.println("OK");
-    break;
-  case GET_ONCE_OUT_PINS:
-    Serial.print("OK ");
-    Serial.println(setOnce);
-    break;
-  case SET_NR_STATES:
-    Serial.println("OK");
-    nrStates = Serial.parseInt();
-    Serial.println("OK");
-    break;
-  case GET_NR_STATES:
-    Serial.print("OK ");
-    Serial.println(nrStates);
-    break;
-  case SET_STATE:
-    {
-    Serial.println("OK");
-    int state = Serial.parseInt();
-    Serial.println("OK");
-    states[state] = Serial.parseInt();
-    Serial.println("OK");
-    delays[state] = Serial.parseInt();
-    Serial.println("OK");
-    }
-    break;
-  case GET_STATE:
-    {
-      Serial.println("OK ");
-    int state = Serial.parseInt();
-    Serial.print("OK ");
-    Serial.print(states[state]);
-    Serial.print(" ");
-    Serial.println(delays[state]);
-    }
-    break;
-  case RESET_ONCE:
-    reset_once();
-    Serial.println("OK");
-    break;
-  case FIRE:
-    fire();
-    Serial.println("OK");
-    break;
-  default:
-    Serial.println("ERROR");
-  break;
+  switch (cmd) {
+    case SET_TRIGGER_WIDTH:
+      Serial.println("OK");
+      trigWidth = Serial.parseInt();
+      //trigWidth = 0;
+      //trigWidth += Serial.read() << 8;
+      //trigWidth += Serial.read();
+      Serial.println("OK");
+      break;
+    case GET_TRIGGER_WIDTH:
+      Serial.print("OK ");
+      Serial.println(trigWidth);
+      break;
+    case SET_TRIGGER_PERIOD:
+      Serial.println("OK");
+      trigPeriod = Serial.parseInt();
+      Serial.println("OK");
+      break;
+    case GET_TRIGGER_PERIOD:
+      Serial.print("OK ");
+      Serial.println(trigPeriod);
+      break;
+    case SET_TRIGGER_OUT_PINS:
+      Serial.println("OK");
+      triggers = Serial.parseInt();
+      Serial.println("OK");
+      break;
+    case GET_TRIGGER_OUT_PINS:
+      Serial.print("OK ");
+      Serial.println(triggers);
+      break;
+    case SET_ONCE_OUT_PINS:
+      Serial.println("OK");
+      setOnce = Serial.parseInt();
+      Serial.println("OK");
+      break;
+    case GET_ONCE_OUT_PINS:
+      Serial.print("OK ");
+      Serial.println(setOnce);
+      break;
+    case SET_NR_STATES:
+      Serial.println("OK");
+      nrStates = Serial.parseInt();
+      Serial.println("OK");
+      break;
+    case GET_NR_STATES:
+      Serial.print("OK ");
+      Serial.println(nrStates);
+      break;
+    case SET_STATE:
+      {
+        Serial.println("OK");
+        int state = Serial.parseInt();
+        Serial.println("OK");
+        states[state] = Serial.parseInt();
+        Serial.println("OK");
+        delays[state] = Serial.parseInt();
+        Serial.println("OK");
+      }
+      break;
+    case GET_STATE:
+      {
+        Serial.println("OK ");
+        int state = Serial.parseInt();
+        Serial.print("OK ");
+        Serial.print(states[state]);
+        Serial.print(" ");
+        Serial.println(delays[state]);
+      }
+      break;
+    case RESET_ONCE:
+      reset_once();
+      Serial.println("OK");
+      break;
+    case FIRE:
+      {
+        Serial.println("OK ");
+        byte first = Serial.parseInt();
+        Serial.println("OK ");
+        byte last = Serial.parseInt();
+        Serial.println("OK ");
+        fire(first, last);
+        Serial.println("OK");
+      }
+      break;
+    case FIRE_N:
+      {
+        Serial.println("OK ");
+        int n = Serial.parseInt();
+        Serial.println("OK ");
+        byte first = Serial.parseInt();
+        Serial.println("OK ");
+        byte last = Serial.parseInt();
+        Serial.println("OK ");
+        fire_n(n, first, last);
+        Serial.println("OK");
+      }
+      break;
+    default:
+      Serial.println("ERROR");
+      break;
   }
 }
